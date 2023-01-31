1186210852590c1173aba08525da2da11c27f7bc
AnacondasFriend <115091100+AnacondasFriend@users.noreply.github.com>
2023-01-31 14:50:12 +0300

    Create index.js

---
+++ задание2/index.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector('.zhmyak');
+btn.addEventListener('click', () =>{
+  width = window.screen.width;
+  height =  window.screen.height;
+  alert(`Ширина вашего экрана равна ${width} а длина ${height}`);
+})
